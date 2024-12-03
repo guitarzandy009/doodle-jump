9f54813979b75f78223a5652af56d068e9882cde
Andy Stuckey <andystuckey@mail.com>
2024-12-03 11:23:02 -0600

    functionality of doodler/ move-left move-right

--- app.js
+++ app.js
@@ -107,19 +107,38 @@ document.addEventListener('DOMContentLoaded', () => {
         if (e.key === "ArrowLeft") {
             moveLeft()
         } else if (e.key === "ArrowRight") {
-            // move right
+            moveRight()
         } else if (e.key === "ArrowUp") {
             // move straight
         }
     }
 
-
     function moveLeft() {
+        if (isGoingRight) {
+            clearInterval(rightTimerId)
+            isGoingRight = false
+        }
         isGoingLeft = true
         leftTimerId = setInterval(function () {
-            doodlerLeftSpace -= 5
-            doodler.style.left = doodlerLeftSpace + 'px'
+            if (doodlerLeftSpace >= 0) {
+                doodlerLeftSpace -= 5
+                doodler.style.left = doodlerLeftSpace + 'px'
+            } else moveRight()
         }, 30)
+    }
+            
+    function moveRight() {
+        if (isGoingLeft) {
+            clearInterval(leftTimerId)
+            isGoingLeft = false
+        }
+        isGoingRight = true
+        rightTimerId = setInterval(function () {
+            if (doodlerLeftSpace <= 340) {
+                doodlerLeftSpace += 5
+                doodler.style.left = doodlerLeftSpace + 'px'
+            } else moveLeft()
+        },30)
     }
 
     function start() {
@@ -137,4 +156,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 })
-// Timestamp 25:50 
+// Timestamp 42:20 
